9b536fae61ceba84853bd4f0b48db0b116f655e7
WANGXING <3551764468@qq.com>
2024-09-18 19:13:40 +0800

    Linux与shell基础2.2

---
+++ Q2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for i in {1..100};do
+    num=`shuf -i 1-100 -n 1`
+    echo $num >> numbers.txt
+done
+
+sum=0
+count=0
+
+while read line
+do
+    sum=$((sum + line))
+    ((count++))
+done < numbers.txt
+
+average=$(bc <<< "scale=2;$sum/$count")
+
+echo "sum:$sum" >> results.json
+echo "average:$average" >> results.json
